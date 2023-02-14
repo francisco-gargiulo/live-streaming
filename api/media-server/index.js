6257135ac60085a2d280ac92a8615f317b406c72
Francisco Gargiulo <francisco_gargiulo@hotmail.com>
2023-02-14 20:55:06 -0300

    updated README.md in media-server

--- api/media-server/index.js
+++ api/media-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 
-const authenticate = require("./middlewares/authenticate");
+const authenticate = require("./middlewares/basic-auth");
 
 const app = express();
 
